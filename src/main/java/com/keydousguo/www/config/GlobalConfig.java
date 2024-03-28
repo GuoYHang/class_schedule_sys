4581b574aded3ae0807f647a37516f0bffb0e128
懒得要死 <1242140285@qq.com>
2024-03-28 16:54:12 +0800

    feat init

--- src/main/java/com/keydousguo/www/config/GlobalConfig.java
+++ src/main/java/com/keydousguo/www/config/GlobalConfig.java
@@ -1,5 +1,7 @@
 package com.keydousguo.www.config;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
@@ -7,79 +9,52 @@ import org.springframework.stereotype.Component;
  * 读取项目相关配置
  */
 
+@Setter
 @Component
 @ConfigurationProperties(prefix = "sys")
 public class GlobalConfig {
-    /** 项目名称 */
+    /**
+     * 项目名称
+     */
+    @Getter
     private String name;
 
-    /** 版本 */
+    /**
+     * 版本
+     */
+    @Getter
     private String version;
 
-    /** 版权年份 */
+    /**
+     * 版权年份
+     */
+    @Getter
     private String copyrightYear;
 
-    /** 上传路径 */
+    /**
+     * 上传路径
+     */
+    @Getter
     private static String profile;
 
-    /** 获取地址开关 */
+    /**
+     * 获取地址开关
+     */
+    @Getter
     private static boolean addressEnabled;
 
-    /** 验证码类型 */
+    /**
+     * 验证码类型
+     */
+    @Getter
     private static String captchaType;
 
-    public String getName()
-    {
-        return name;
-    }
-
-    public void setName(String name)
-    {
-        this.name = name;
-    }
-
-    public String getVersion()
-    {
-        return version;
-    }
-
-    public void setVersion(String version)
-    {
-        this.version = version;
-    }
-
-    public String getCopyrightYear()
-    {
-        return copyrightYear;
-    }
-
-    public void setCopyrightYear(String copyrightYear)
-    {
-        this.copyrightYear = copyrightYear;
-    }
-
-    public static String getProfile()
-    {
-        return profile;
-    }
-
-    public void setProfile(String profile)
-    {
+    public void setProfile(String profile) {
         GlobalConfig.profile = profile;
     }
 
-    public static boolean isAddressEnabled()
-    {
-        return addressEnabled;
-    }
-
-    public void setAddressEnabled(boolean addressEnabled)
-    {
+    public void setAddressEnabled(boolean addressEnabled) {
         GlobalConfig.addressEnabled = addressEnabled;
-    }
-
-    public static String getCaptchaType() {
-        return captchaType;
     }
 
     public void setCaptchaType(String captchaType) {
@@ -89,32 +64,28 @@ public class GlobalConfig {
     /**
      * 获取导入上传路径
      */
-    public static String getImportPath()
-    {
+    public static String getImportPath() {
         return getProfile() + "/import";
     }
 
     /**
      * 获取头像上传路径
      */
-    public static String getAvatarPath()
-    {
+    public static String getAvatarPath() {
         return getProfile() + "/avatar";
     }
 
     /**
      * 获取下载路径
      */
-    public static String getDownloadPath()
-    {
+    public static String getDownloadPath() {
         return getProfile() + "/download/";
     }
 
     /**
      * 获取上传路径
      */
-    public static String getUploadPath()
-    {
+    public static String getUploadPath() {
         return getProfile() + "/upload";
     }
 }
